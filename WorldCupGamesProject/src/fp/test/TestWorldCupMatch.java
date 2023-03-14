1a1e0a344706a9e3e77bbbeff495bd8beb87c221
OscarMLB24 <116158556+OscarMLB24@users.noreply.github.com>
2023-03-14 19:51:28 +0100

    Add files via upload

--- WorldCupGamesProject/src/fp/test/TestWorldCupMatch.java
+++ WorldCupGamesProject/src/fp/test/TestWorldCupMatch.java
@@ -1,17 +1,34 @@
 package fp.test;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import fp.types.Location;
 import fp.types.WorldCupMatch;
 
 public class TestWorldCupMatch {
 	public static void main(String[] args) {
-		WorldCupMatch m =  new WorldCupMatch(LocalDate.now(), "Final", "Argentina", 3, 3, "France");
-		WorldCupMatch n =  new WorldCupMatch(LocalDate.now(), "Semifinal","Benito Villamarín","Sevilla", "Argentina", 3, 3, "Holland", "Mateu Lahoz");
-		System.out.println(m.shortFormat());
+		//WorldCupMatch w = new WorldCupMatch("1930;13 Jul 1930 - 15:00;Group 1;Pocitos;Montevideo;France;4;1;Mexico; ;4444;3;0;LOMBARDI Domingo (URU);CRISTOPHE Henry (BEL);REGO Gilberto (BRA);201;1096;FRA;MEX;6,26");
+		Location l = new Location("Pocitos","Monteviedo");
+		List<String> list = new ArrayList<String>();
+		WorldCupMatch m = new WorldCupMatch(1930,LocalDateTime.of(1930, 07, 12, 15, 0), "Group 1" ,l, "France" , 4, 1, "Mexico", "", 4444, 3, 0, list , 201, 1096, "FRA", "MEX", 6.26);
+		WorldCupMatch n = new WorldCupMatch(LocalDateTime.of(1930, 07, 12, 15, 0), "Group 4", "USA", 3, 0, "Belgium", 1090);
 		System.out.println(m.toString());
-		System.out.println(m.getRecent());
+		m.setHomeTeamGoals(2);
+		m.setAwayTeamGoals(2);
+		m.setHalfTimeHomeGoals(1);
+		m.setHalfTimeAwayGoals(0);
+		m.setHomeTeamInitials("FRN");
+		m.setAwayTeamInitials("MXC");
+		System.out.println(m.toString());
+		System.out.println(m.shortFormat());
+		System.out.println(m.hashCode());
 		System.out.println(m.equals(n));
 		System.out.println(m.compareTo(n));
+		m.addReferee("LOMBARDI Domingo (URU)");
+		System.out.println(m.toString());
+		m.removeReferee();
+		System.out.println(m.toString());
 	}
 }
---
+++ WorldCupGamesProject/src/fp/types/Location.java
@@ -0,0 +1,93 @@
+package fp.types;
+
+import java.util.Objects;
+
+import fp.utils.Checkers;
+
+public class Location{
+
+	//properties
+
+	private String stadium;
+	private String city;
+
+	//constructors
+
+	public Location() {
+		stadium = "";
+		city = "";
+	}
+
+	public Location(String stadium, String city) {
+		Checkers.check("Not allowed values", stadium!=null && city!=null);
+		this.stadium = stadium;
+		this.city = city;
+	}
+
+	public Location(String s) {
+		String[] values = s.split(";");
+		Checkers.check("Not valid format", values.length==2);
+		
+		String stadium = String.valueOf(values[0].replace("(", "").trim());
+		String city = String.valueOf(values[1].replace(")", "").trim());
+		
+		this.stadium = stadium;
+		this.city = city;
+	}
+	
+	//getters and setters
+
+	public String getStadium() {
+		return stadium;
+	}
+
+	public void setStadium(String stadium) {
+		Checkers.check("Not allowed values", stadium!=null);
+		this.stadium = stadium;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		Checkers.check("Not allowed values", city!=null);
+		this.city = city;
+	}
+	
+	//string conversion
+	
+	@Override
+	public String toString() {
+		return "Location [stadium=" + stadium + ", city=" + city + "]";
+	}
+	
+	//equality criteria
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, stadium);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		return Objects.equals(city, other.city) && Objects.equals(stadium, other.stadium);
+	}
+	
+	//natural order criteria
+	
+	public Integer compareTo(Location l) {
+		int res = getCity().compareTo(l.getCity());
+		if (res==0) {
+			res = getStadium().compareTo(l.getStadium());
+		}
+		return res;
+	}
+}
