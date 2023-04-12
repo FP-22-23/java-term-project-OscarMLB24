e6fe3c588d6099464110de152fd34df9ca5dd765
OscarMLB24 <116158556+OscarMLB24@users.noreply.github.com>
2023-04-12 10:06:35 +0200

    Update TestWorldCupMatch.java

--- WorldCupGamesProject/src/fp/test/TestWorldCupMatch.java
+++ WorldCupGamesProject/src/fp/test/TestWorldCupMatch.java
@@ -9,7 +9,7 @@ import fp.types.WorldCupMatch;
 
 public class TestWorldCupMatch {
 	public static void main(String[] args) {
-		Location l = new Location("Pocitos","Monteviedo");
+		/*Location l = new Location("Pocitos","Monteviedo");
 		List<String> list = new ArrayList<String>();
 		WorldCupMatch m = new WorldCupMatch(1930,LocalDateTime.of(1930, 07, 12, 15, 0), "Group 1" ,l, "France" , 4, 1, "Mexico", "", 4444, 3, 0, list , 201, 1096, "FRA", "MEX", 6.26);
 		WorldCupMatch n = new WorldCupMatch(LocalDateTime.of(1930, 07, 12, 15, 0), "Group 4", "USA", 3, 0, "Belgium", 1090);
@@ -28,6 +28,9 @@ public class TestWorldCupMatch {
 		m.addReferee("LOMBARDI Domingo (URU)");
 		System.out.println(m.toString());
 		m.removeReferee();
-		System.out.println(m.toString());
+		System.out.println(m.toString());*/
+		
+		Matches matches = MatchFactory.readMatches("data/WorldCupMatches.csv");
+		System.out.println(matches.getNumberMatches());
 	}
 }
