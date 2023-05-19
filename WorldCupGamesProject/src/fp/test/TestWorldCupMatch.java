2b09b76affc304f1681edc06498738860550c187
OscarMLB24 <116158556+OscarMLB24@users.noreply.github.com>
2023-05-19 22:36:16 +0200

    Add files via upload

---
+++ WorldCupGamesProject/src/fp/test/TestWorldCupMatch.java
@@ -0,0 +1,338 @@
+package fp.test;
+
+import java.util.Scanner;
+
+import fp.types.MatchFactory;
+import fp.types.Matches;
+
+public class TestWorldCupMatch {
+	
+	static Matches matches = MatchFactory.readMatches("data/WorldCupMatches.csv");
+	
+	public static void main(String[] args) {
+
+		running();
+	}
+	
+	private static void welcome() {
+		 
+		System.out.println("\n  Hello, this is my project about all the World Cup matches of football's history."
+				+"\n\n	0.  End program." 
+				+"\n	1.  Get an specific match."
+				+ "\n	2.  Get a range of matches ordered by date."
+				+ "\n	3.  Get total number of matches."
+				+ "\n	4.  Check if there is a match with a specific characteristic."
+				+ "\n	5.  Calculate the average of a numeric characteristic."
+				+ "\n	6.  Filter the list of matches by an specific characteristic."
+				+ "\n	7.  Get a dictionary which relates a country to the matches it has played."
+				+ "\n	8.  Get a dictionary which relates a country to the number of won matches."
+				+ "\n	9.  Get a match with a maximum or minimum characteristic after having been filtered by another characteristic."
+				+ "\n	10. Get a list of matches sorted by a characteristic after having been filtered by another characteristic."
+				+ "\n	11. Get all the matches in a short format."
+				+ "\n	12. Get a dictionary which relates a year to the greatest attendance of that World Cup."
+				+ "\n	13. Get a dictionary which relates a country to a list with the n games of that team where there have been more total goals."
+				+ "\n	14. Get the team with the most or least won matches."
+				+ "\n	15. Get all the winners of the World Cup each year.");
+	}
+	
+	private static void running() {
+		
+		Boolean run = true;
+		
+		while(run) {
+			
+			welcome();
+			Integer n = askForInteger("What would you want to do? (number)\n",0,16);
+			
+			if(n == 0) {
+				
+				break;
+			}
+			else if(n == 1) {
+				
+				testF1();
+			}
+			else if(n == 2) {
+				
+				testF2();
+			}
+			else if(n == 3) {
+				
+				testF3();
+			}
+			else if(n == 4) {
+				
+				testF4();
+			}
+			else if(n == 5) {
+				
+				testF5();
+			}
+			else if(n == 6) {
+				
+				testF6();
+			}
+			else if(n == 7) {
+				
+				testF7();
+			}
+			else if(n == 8) {
+				
+				testF8();
+			}
+			else if(n == 9) {
+				
+				testF9();
+			}
+			else if(n == 10) {
+				
+				testF10();
+			}
+			else if(n == 11) {
+				
+				testF11();
+			}
+			else if(n == 12) {
+				
+				testF12();
+			}
+			else if(n == 13) {
+				
+				testF13();
+			}
+			else if(n == 14) {
+				
+				testF14();
+			}
+			else if(n == 15) {
+				 
+				testF15();
+			}
+		}
+	}
+
+	@SuppressWarnings("resource")
+	private static String askForString(String question) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("\n  " + question);
+		
+		return scanner.nextLine();
+		
+	}
+
+	@SuppressWarnings("resource")
+	private static Integer askForInteger(String question, Integer interA, Integer interB) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("\n  " + question);
+		
+		try {
+			
+			String res = scanner.nextLine();
+			
+			if(Integer.valueOf(res) >= interA && Integer.valueOf(res) < interB){
+				
+				return Integer.valueOf(res);
+			}
+			
+			else {
+				
+				return askForInteger(question,interA,interB);
+			}
+		}
+		catch(NumberFormatException e) {
+			
+			return askForInteger(question,interA,interB);
+		}
+	}
+	
+	private static void testF1() {
+		
+		Integer i = askForInteger("Which match do you want to see? (number)\n",1,matches.getNumberMatches()+1);
+		
+		System.out.println("\n   * " + matches.getMatch(i-1));
+	}
+
+	private static void testF2() {
+		
+		Integer i = askForInteger("Which is the first match position do you want to see? (number)\n",1,matches.getNumberMatches());
+		Integer e = askForInteger("Which is the last match position do you want to see? (number)\n",1,matches.getNumberMatches()+1);
+		
+		System.out.println("\n   * " + matches.getMatches(i-1,e));
+	}
+	
+	private static void testF3() {
+		
+		System.out.println("\n   * " + matches.getNumberMatches());
+	}
+	
+	private static void testF4() {
+		
+		System.out.println("\n The variables to check are:"
+				+ "\n\n	1.  year"
+				+ "\n	2.  homeTeamGoals"
+				+ "\n	3.  awayTeamGoals"
+				+ "\n	4.  attendance"
+				+ "\n	5.  halfTimeHomeGoals"
+				+ "\n	6.  halfTimeAwayGoals"
+				+ "\n	7.  matchId"
+				+ "\n	8.  roundId"
+				+ "\n	9.  stage"
+				+ "\n	10. homeTeamName"
+				+ "\n	11. awayTeamName"
+				+ "\n	12. homeTeamInitials"
+				+ "\n	13. awayTeamInitials"
+				+ "\n	14. firstShotMinute"
+				+ "\n	15. location (for the value write: stadium,city)"
+				+ "\n	16. referees"
+				+ "\n	17. date (example for the value: 13 Jul 1930)" 
+				+ "\n	18. time (example for the value: 21:00)");
+		
+		String var = askForString("\n Which variable do you want to use? (exact name)\n");
+		String val = askForString("\n Which value do you want to check?\n");
+		
+		System.out.println("\n   * " + matches.checkMatchData(var,val));
+	}
+	
+	private static void testF5() {
+		
+		System.out.println("\n The variables to d the average are:\n"
+				+ "\n	1.  homeTeamGoals"
+				+ "\n	2.  awayTeamGoals"
+				+ "\n	3.  attendance"
+				+ "\n	4.  halfTimeHomeGoals"
+				+ "\n	5.  halfTimeAwayGoals"
+				+ "\n	6.  firstShotMinute");
+		
+		String var = askForString("\n Which variable do you want to use? (exact name)\n");
+		
+		System.out.println("\n   * " + matches.averageValue(var));
+	}
+	
+	private static void testF6() {
+		
+		System.out.println("\n The variables to filter are:"
+				+ "\n\n	1.  year"
+				+ "\n	2.  homeTeamGoals"
+				+ "\n	3.  awayTeamGoals"
+				+ "\n	4.  attendance"
+				+ "\n	5.  halfTimeHomeGoals"
+				+ "\n	6.  halfTimeAwayGoals"
+				+ "\n	7.  matchId"
+				+ "\n	8.  roundId"
+				+ "\n	9.  stage"
+				+ "\n	10. homeTeamName"
+				+ "\n	11. awayTeamName"
+				+ "\n	12. homeTeamInitials"
+				+ "\n	13. awayTeamInitials"
+				+ "\n	14. firstShotMinute"
+				+ "\n	15. location (for the value write: stadium,city)"
+				+ "\n	16. referees"
+				+ "\n	17. date (example for the value: 13 Jul 1930)"
+				+ "\n	18. time (example for the value: 21:00)");
+		
+		String var = askForString("\n Which variable do you want to use? (exact name)\n");
+		String val = askForString("\n Which value do you want to check?\n");
+		
+		System.out.println("\n   * " + matches.filterBy(var,val));
+	}
+	
+	private static void testF7() {
+		
+		System.out.println("\n   * " + matches.matchesPerCountry());
+	}
+	
+	private static void testF8() {
+		
+		System.out.println("\n   * " + matches.numberWonMatchesPerCountry());
+	}
+	
+	private static void testF9() { // NO FUNCIONA CON FILTER DE YEAR Y MAX/MIN DE ATTENDANCE
+		
+		System.out.println("\n The variables to filter are:"
+				+ "\n\n	1.  year"
+				+ "\n	2.  homeTeamGoals"
+				+ "\n	3.  awayTeamGoals"
+				+ "\n	4.  attendance"
+				+ "\n	5.  halfTimeHomeGoals"
+				+ "\n	6.  halfTimeAwayGoals"
+				+ "\n	7.  matchId"
+				+ "\n	8.  roundId"
+				+ "\n	9.  stage"
+				+ "\n	10. homeTeamName"
+				+ "\n	11. awayTeamName"
+				+ "\n	12. homeTeamInitials"
+				+ "\n	13. awayTeamInitials"
+				+ "\n	14. firstShotMinute"
+				+ "\n	15. location (for the value write: stadium,city)"
+				+ "\n	16. referees"
+				+ "\n	17. date (example for the value: 13 Jul 1930)"
+				+ "\n	18. time (example for the value: 21:00)");
+		
+		String filterVar = askForString("\n Which variable do you want to use to filter? (exact name)\n");
+		String filterVal = askForString("\n Which value do you want to filter?\n");
+		String maxMinVar = askForString("\n Which variable do you want to use to maximize/minimize? (exact name)\n");
+		String funct = askForString("\n Do you want to maximize or minimize (write: max/min)?\n");
+		
+		System.out.println("\n   * " + matches.maxOrMinAfterFiltering(filterVar,filterVal,maxMinVar,funct));
+	}
+	
+	private static void testF10() {
+		
+		System.out.println("\n The variables to filter are:"
+				+ "\n\n	1.  year"
+				+ "\n	2.  homeTeamGoals"
+				+ "\n	3.  awayTeamGoals"
+				+ "\n	4.  attendance"
+				+ "\n	5.  halfTimeHomeGoals"
+				+ "\n	6.  halfTimeAwayGoals"
+				+ "\n	7.  matchId"
+				+ "\n	8.  roundId"
+				+ "\n	9.  stage"
+				+ "\n	10. homeTeamName"
+				+ "\n	11. awayTeamName"
+				+ "\n	12. homeTeamInitials"
+				+ "\n	13. awayTeamInitials"
+				+ "\n	14. firstShotMinute"
+				+ "\n	15. location (for the value write: stadium,city)"
+				+ "\n	16. referees"
+				+ "\n	17. date (example for the value: 13 Jul 1930)"
+				+ "\n	18. time (example for the value: 21:00)");
+		
+		String filterVar = askForString("\n Which variable do you want to use to filter? (exact name)\n");
+		String filterVal = askForString("\n Which value do you want to filter?\n");
+		String sortVar = askForString("\n Which variable do you want to use to sort? (exact name)\n");
+		String reversed = askForString("\n Do you want reversed order? (write: yes/no)\n");
+	
+		System.out.println("\n   * " + matches.sortedFiltering(filterVar,filterVal,sortVar,reversed));
+	}
+	
+	private static void testF11() {
+		
+		System.out.println("\n   * " + matches.printMatches());
+	}
+	
+	private static void testF12() {
+		
+		System.out.println("\n   * " + matches.maxAttendanceEachYear());
+	}
+	
+	private static void testF13() {
+		
+		Integer n = askForInteger("How many matches do you want to see?\n",0,matches.getNumberMatches());
+		System.out.println("\n   * " + matches.matchesWithMoreTotalGoalsPerCountry(n));
+	}
+	
+	private static void testF14() {
+		
+		String funct = askForString("\n Do you want to maximize or minimize (write: max/min)?\n");
+		System.out.println("\n   * " + matches.countryWithMostWonMatches(funct));
+	}
+	
+	private static void testF15() {
+		
+		System.out.println("\n   * " + matches.worldCupWinnerPerYear());
+	}
+}
---
+++ WorldCupGamesProject/src/fp/types/Match.java
@@ -0,0 +1,338 @@
+package fp.types;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import fp.utils.Checkers;
+
+public class Match implements Comparable<Match> {
+
+	// properties
+
+	private Integer year;
+	private LocalDateTime dateTime;
+	private String stage;
+	private Location location;
+	private String homeTeamName;
+	private Integer homeTeamGoals;
+	private Integer awayTeamGoals;
+	private String awayTeamName;
+	private String winConditions;
+	private Integer attendance;
+	private Integer halfTimeHomeGoals;
+	private Integer halfTimeAwayGoals;
+	private List<String> referees; //Main referee, assistant1, assistant2
+	private Integer roundId;
+	private Integer matchId;
+	private String homeTeamInitials;
+	private String awayTeamInitials;
+	private Double firstShotMinute;
+
+	// constructors
+	
+	// 1. With all parameters
+	
+	public Match(Integer year,LocalDateTime dateTime, String stage, Location location, String homeTeamName, Integer homeTeamGoals, Integer awayTeamGoals, String awayTeamName,String winConditions, Integer attendance, Integer halfTimeHomeGoals, Integer halfTimeAwayGoals, List<String> referees, Integer roundId, Integer matchId, String homeTeamInitials, String awayTeamInitials, Double firstShotMinute) {
+		
+		if(year != null) {	
+			Checkers.check("The year is not correct", year>=1930 && year<=LocalDate.now().getYear() && (year-1930)%4==0 && year==dateTime.getYear());
+		}
+
+		if(homeTeamGoals != null) {
+			Checkers.check("The home team goals are not correct", homeTeamGoals>=0);
+		}
+		
+		if(awayTeamGoals != null) {
+			Checkers.check("The away team goals are not correct", awayTeamGoals>=0);
+		}
+
+		if(attendance != null) {
+			Checkers.check("The attendace is not correct", attendance>=0);
+		}
+
+		if(halfTimeHomeGoals != null) {
+			Checkers.check("The half-time home team goals are not correct", halfTimeHomeGoals>=0 && halfTimeHomeGoals<=homeTeamGoals);
+		}
+
+		if(halfTimeAwayGoals != null) {
+			Checkers.check("The half-time away team goals are not correct", halfTimeAwayGoals>=0 && halfTimeAwayGoals<=awayTeamGoals);
+		}
+		
+		if(roundId != null) {
+			Checkers.check("The round id is not correct", roundId>=0);
+		}
+		
+		if(matchId != null) {
+			Checkers.check("The match id is not correct", matchId>=0);
+		}
+
+		if(firstShotMinute != null) {
+			Checkers.check("The minute of the first shot is not correct", firstShotMinute>=0.0);
+		}
+
+		Checkers.check("The date is not correct", !dateTime.isAfter(LocalDateTime.now()));
+		Checkers.check("The initials of the teams are not correct", homeTeamInitials.length()==3 && awayTeamInitials.length()==3);
+
+		this.year = year;
+		this.dateTime = dateTime;
+		this.stage = stage;
+		this.location= location;
+		this.homeTeamName = homeTeamName;
+		this.homeTeamGoals = homeTeamGoals;
+		this.awayTeamGoals = awayTeamGoals;
+		this.awayTeamName = awayTeamName;
+		this.winConditions = winConditions;
+		this.attendance = attendance;
+		this.halfTimeHomeGoals = halfTimeHomeGoals;
+		this.halfTimeAwayGoals = halfTimeAwayGoals;
+		this.referees = referees;
+		this.roundId = roundId;
+		this.matchId = matchId;
+		this.homeTeamInitials = homeTeamInitials;
+		this.awayTeamInitials = awayTeamInitials;
+		this.firstShotMinute = firstShotMinute;
+	}
+
+	// 2. With some parameters
+	
+	public Match(LocalDateTime dateTime, String stage, String homeTeamName, Integer homeTeamGoals, Integer awayTeamGoals, String awayTeamName, Integer matchId) {
+
+		if(year != null) {	
+			Checkers.check("The year is not correct", year>=1930 && year<=LocalDate.now().getYear() && (year-1930)%4==0 && year==dateTime.getYear());
+		}
+		if(homeTeamGoals != null) {
+			Checkers.check("The home team goals are not correct", homeTeamGoals>=0);
+		}
+		if(awayTeamGoals != null) {
+			Checkers.check("The away team goals are not correct", awayTeamGoals>=0);
+		}
+		if(matchId != null) {
+			Checkers.check("The match id is not correct", matchId>=0);
+		}
+
+		this.dateTime = dateTime;
+		this.stage = stage;
+		this.homeTeamName = homeTeamName;
+		this.homeTeamGoals = homeTeamGoals;
+		this.awayTeamGoals = awayTeamGoals;
+		this.awayTeamName = awayTeamName;
+		this.matchId = matchId;
+	}
+
+	// derived properties
+
+	public MatchResult getResult() {
+		
+		MatchResult res = MatchResult.DRAW;
+		if (homeTeamGoals > awayTeamGoals) {
+			
+			return MatchResult.LOCAL_WINS;
+		}
+		else if (homeTeamGoals < awayTeamGoals) {
+			
+			return MatchResult.VISITANT_WINS;
+		}
+		return res;	
+	}
+
+	public Boolean getRecent() {
+		
+		Boolean res = true;
+		if (dateTime.isBefore(LocalDateTime.of(2000, 1, 1, 0, 0))) {
+			
+			return false;
+		}
+		return res;
+	}
+	
+	public Integer getTotalGoals() {
+		
+		return homeTeamGoals + awayTeamGoals;
+	}
+
+	// getters and setters
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public String getStage() {
+		return stage;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public String getHomeTeamName() {
+		return homeTeamName;
+	}
+
+	public Integer getHomeTeamGoals() {
+		return homeTeamGoals;
+	}
+
+	public Integer getAwayTeamGoals() {
+		return awayTeamGoals;
+	}
+
+	public String getAwayTeamName() {
+		return awayTeamName;
+	}
+
+	public String getWinConditions() {
+		return winConditions;
+	}
+
+	public Integer getAttendance() {
+		return attendance;
+	}
+
+	public Integer getHalfTimeHomeGoals() {
+		return halfTimeHomeGoals;
+	}
+
+	public Integer getHalfTimeAwayGoals() {
+		return halfTimeAwayGoals;
+	}
+
+	public List<String> getReferees() {
+		return referees;
+	}
+
+	public Integer getRoundId() {
+		return roundId;
+	}
+
+	public Integer getMatchId() {
+		return matchId;
+	}
+
+	public String getHomeTeamInitials() {
+		return homeTeamInitials;
+	}
+
+	public String getAwayTeamInitials() {
+		return awayTeamInitials;
+	}
+
+	public Double getFirstShotMinute() {
+		return firstShotMinute;
+	}
+	
+	public void setHomeTeamGoals(Integer homeTeamGoals) {
+		
+		Checkers.check("The goals are not correct", homeTeamGoals>=0);
+		this.homeTeamGoals = homeTeamGoals;
+	}
+
+	public void setAwayTeamGoals(Integer awayTeamGoals) {
+		
+		Checkers.check("The goals are not correct", awayTeamGoals>=0);
+		this.awayTeamGoals = awayTeamGoals;
+	}
+
+	public void setWinConditions(String winConditions) {
+		
+		this.winConditions = winConditions;
+	}
+
+	public void setHalfTimeHomeGoals(Integer halfTimeHomeGoals) {
+		
+		Checkers.check("The goals are not correct", halfTimeHomeGoals>=0);
+		this.halfTimeHomeGoals = halfTimeHomeGoals;
+	}
+
+	public void setHalfTimeAwayGoals(Integer halfTimeAwayGoals) {
+		
+		Checkers.check("The goals are not correct", halfTimeAwayGoals>=0);
+		this.halfTimeAwayGoals = halfTimeAwayGoals;
+	}
+
+	public void setHomeTeamInitials(String homeTeamInitials) {
+		
+		Checkers.check("The initials of the team are not correct", homeTeamInitials.length()==3);
+		this.homeTeamInitials = homeTeamInitials;
+	}
+
+	public void setAwayTeamInitials(String awayTeamInitials) {
+		
+		Checkers.check("The initials of the team are not correct", awayTeamInitials.length()==3);
+		this.awayTeamInitials = awayTeamInitials;
+	}
+	
+	// string representation
+
+	@Override
+	public String toString() {
+		
+		return "WorldCupMatch [year=" + year + ", dateTime=" + dateTime + stage + ", location="
+				+ location + ", homeTeamName=" + homeTeamName + ", homeTeamGoals=" + homeTeamGoals + ", awayTeamGoals="
+				+ awayTeamGoals + ", awayTeamName=" + awayTeamName + ", winConditions=" + winConditions
+				+ ", attendance=" + attendance + ", halfTimeHomeGoals=" + halfTimeHomeGoals + ", halfTimeAwayGoals="
+				+ halfTimeAwayGoals + ", referees=" + referees + ", roundId=" + roundId + ", matchId=" + matchId
+				+ ", homeTeamInitials=" + homeTeamInitials + ", awayTeamInitials=" + awayTeamInitials
+				+ ", firstShotMinute=" + firstShotMinute + ", result=" + getResult() + ", recent=" + getRecent() + ", totalGoals=" + getTotalGoals() + "]";
+	}
+	
+	public String shortFormat() {
+		
+		return stage.toUpperCase() + "(" + dateTime.getYear() + ") : " + homeTeamName + "(" + homeTeamInitials.toUpperCase() + ")" + " " + homeTeamGoals + "-" + awayTeamGoals + " " + awayTeamName + "(" + awayTeamInitials.toUpperCase() + ")";
+	}
+	
+	// equality criteria
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(awayTeamName, homeTeamName, stage, year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Match other = (Match) obj;
+		return Objects.equals(awayTeamName, other.awayTeamName) && Objects.equals(homeTeamName, other.homeTeamName)
+				&& Objects.equals(stage, other.stage) && Objects.equals(year, other.year);
+	}
+
+	// natural order criteria
+	
+	public int compareTo(Match m) {
+
+		Integer res = getDateTime().compareTo(m.getDateTime());
+		return res;
+	}
+
+	// Methods
+
+	// 1. Adds a referee to the list of them
+
+	public void addReferee(String r) {
+		referees.add(r);
+	}
+
+	// 2. Removes a referee from the list of them
+	
+	public void removeReferee() {
+		
+		if (referees.size()>1){
+			
+			referees.remove(-1);
+		}
+		else if(referees.size()==1){
+			
+			referees.remove(0);
+		}
+	}
+}
